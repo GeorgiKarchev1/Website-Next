1890250169f7b72734640b8c3bbe82b20385d17f
Karchev <georgikarchev5@gmail.com>
2024-03-21 11:42:00 -0700

    Fixing landing page and make it looks good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
+"use client";
 import React from "react";
-import { cn } from "@/app/utils/cn";
-import { Spotlight } from "@/app/components/ui/Spotlight";
+import { AuroraBackground } from "@/app/components/ui/aurora-background";
+import { motion } from "framer-motion";
 
-export function SpotlightPreview() {
+export function AuroraBackgroundDemo() {
   return (
-    <div className="h-[45rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 ">
-        <h1 className="md:text-7xl  font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 ">
-          Georgi Karchev 
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Developer who like to create usefull software
-        </p>
-        </h1>
-      </div>
-    </div>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          Georgi Karchev
+        </div>
+        <div className="font-extralight text-base md:text-3xl dark:text-neutral-200 py-4">
+          I like to create usefull software!
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Contact now
+        </button>
+      </motion.div>
+    </AuroraBackground>
   );
 }
-export default SpotlightPreview;
+
+export default AuroraBackgroundDemo;
